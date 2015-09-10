62288c898190bc449257614194044c187cf0a19e
ConnerO <conneroheeron@gmail.com>
2015-09-10 10:15:15 -0500

    Got the X's to appear.

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+
+// use an if statement for the turn numbers.
+// onClick add a number to a counter.
+
+	$(".button").click(function(){
+	
+
+		$(this).text("X")
+	});
+});
